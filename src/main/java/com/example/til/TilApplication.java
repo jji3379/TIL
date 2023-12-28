0392c72594eecd225dd05c3730794e29a128dbea
jji3379 <jji3379@naver.com>
2023-12-28 12:55:24 +0900

    Initial commit

---
+++ src/main/java/com/example/til/TilApplication.java
@@ -0,0 +1,15 @@
+package com.example.til;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+
+@SpringBootApplication
+public class TilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TilApplication.class, args);
+    }
+
+}
